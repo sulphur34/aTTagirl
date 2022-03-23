c83603ec0ee27859f92a83ab5f6c1111cfcdb3c1
Александр Ивашев <toku_gawa@mail.ru>
2022-03-23 14:14:20 +0500

    Добавьте файлы проекта.

---
+++ aTTagirl/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aTTagirl
+{
+    struct Player
+    {
+        string playerID;
+        List<Score> scores;             
+    }
+    struct Score
+    {
+        string ScoreName;
+        int ScoreCount;
+    }
+}
